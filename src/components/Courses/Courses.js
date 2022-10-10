a21bedcd98bfaafc00c42667de161742669f36d0
sr-sony <sr.sony.05@gmail.com>
2022-10-11 01:54:42 +0600

    api loaded

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = ({courseData}) => {
+    const {name, logo, id, total} = courseData;
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Courses;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Courses from '../Courses/Courses';
 
 const Home = () => {
+    const course = useLoaderData();
+    const courseDatas = course.data;
     return (
         <div>
             <h1>This is Home</h1>
+            {
+                courseDatas.map(courseData => <Courses key={courseData.id} courseData={courseData} >
+
+                </Courses>)
+            }
+            
         </div>
     );
 };
