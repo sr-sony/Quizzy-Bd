04ddae06c99b66996a80221e2be13422bcf94ecf
sr-sony <sr.sony.05@gmail.com>
2022-10-12 21:21:29 +0600

    heading added

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import img from '../../images/heading.jpg'
+import './Heading.css';
 const Heading = () => {
     return (
-        <div>
-            <h1>This is heading</h1>
+        <div className='heading-container'>
+            <img src={img} alt=""></img>
+            <h2>Welcome to Quizzy BD</h2>
+            <p>Here you can test your knowledge by completing some quiz test and you will get the result.</p>
+            <h5>Let's Go!!!</h5>
         </div>
     );
 };
